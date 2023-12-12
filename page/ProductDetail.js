c85d55581632b1ee0c69a2cd9050840973fab037
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2023-12-12 17:35:06 +0900

    cng

--- page/ProductDetail.js
+++ page/ProductDetail.js
@@ -49,8 +49,7 @@ const ProductDetail = ({basicUrl}) => {
     const temp = new Date(enddate.substr(0, 10)+' '+enddate.substr(11, 8)); 
     if(temp.getTime() - now.getTime() < 0)
       return '종료';
-    return ' 종료까지  ' + Math.floor((temp.getTime() - now.getTime()) / (1000*60*60*24)) + '일 ' + Math.floor(((temp.getTime() - now.getTime()) % (1000*60*60*24)) / (1000*60*60))
-    + '시간 남음';
+    return Math.floor((temp.getTime() - now.getTime()) / (1000*60*60*24));
   }
   
   const handleTime = (regdate) => {
@@ -69,7 +68,7 @@ const ProductDetail = ({basicUrl}) => {
   }
 
   return (
-    <div style={{width:'100%', backgroundColor:'#D0F0E3'}}>
+    <div style={{width:'100vw', backgroundColor:'#D0F0E3'}}>
       <div style={{textAlign:'center', width:'100%', margin:'0 auto'}}>
       {details&&
         <div>
@@ -105,21 +104,21 @@ const ProductDetail = ({basicUrl}) => {
             <hr/>
             
             {/* 펀딩 정보 */}
-            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw'}}>
-              <span style={{float:'left', color:'#bbbbbb', width:'20vw', textAlign:'start'}}>카테고리</span>
+            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw', fontSize:'2vh'}}>
+              <span style={{float:'left', color:'#777777', width:'20vw', textAlign:'start'}}>카테고리</span>
               <span style={{float:'left'}}>{details.category}</span>
             </div>
-            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw'}}>
-              <span style={{float:'left', color:'#bbbbbb', width:'20vw', textAlign:'start'}}>상품명</span>
+            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw', fontSize:'2vh'}}>
+              <span style={{float:'left', color:'#777777', width:'20vw', textAlign:'start'}}>상품명</span>
               <span style={{float:'left'}}>{details.option}</span>
             </div>
-            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw'}}>
-              <span style={{float:'left', color:'#bbbbbb', width:'20vw', textAlign:'start'}}>프딩일</span>
+            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw', fontSize:'2vh'}}>
+              <span style={{float:'left', color:'#777777', width:'20vw', textAlign:'start'}}>프딩일</span>
               <span style={{float:'left'}}>{details.regdate.substr(0,10)}</span>
             </div>
-            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw'}}>
-              <span style={{float:'left', color:'#bbbbbb', width:'20vw', textAlign:'start'}}>마감일</span>
-              <span style={{float:'left'}}>🗓️{details.enddate.substr(0, 10)} / {handleDetailDate(details.enddate)}</span>
+            <div className='font-content' style={{width:'90vw', marginTop:'1vh', height:'3vh', marginLeft:'3vw', fontSize:'2vh'}}>
+              <span style={{float:'left', color:'#777777', width:'20vw', textAlign:'start'}}>마감일</span>
+              <span style={{float:'left'}}>🗓️{details.enddate.substr(0, 10)} / 종료까지 <span style={{color:'red'}}>{handleDetailDate(details.enddate)}</span> 일 남음</span>
             </div>
 
             <hr/>
